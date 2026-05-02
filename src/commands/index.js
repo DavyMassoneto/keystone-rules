ffb7bd5f703a425a9ce0ebf1ad7a47bd17214491
DavyMassoneto <davy.massoneto@yahoo.com>
2026-05-02 11:16:48 -0300

    feat(commands): export commands registry dict from barrel

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,2 +1,10 @@
 export * from './version.js';
 export * from './help.js';
+
+import { versionCommand } from './version.js';
+import { helpCommand } from './help.js';
+
+export const commands = {
+  [versionCommand.name]: versionCommand,
+  [helpCommand.name]: helpCommand,
+};
