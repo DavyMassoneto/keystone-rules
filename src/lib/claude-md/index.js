be13401afd4b4b99a666d728595490e77344f815
DavyMassoneto <davy.massoneto@yahoo.com>
2026-05-02 17:57:32 -0300

    feat: add validateRule

--- src/lib/claude-md/index.js
+++ src/lib/claude-md/index.js
@@ -2,3 +2,4 @@ export * from './is-non-empty-array.js';
 export * from './is-non-empty-string.js';
 export * from './is-object.js';
 export * from './validate-rule-example.js';
+export * from './validate-rule.js';
