d734ac2d383138584b1bb6ffba4cacdb47d93fc8
DavyMassoneto <davy.massoneto@yahoo.com>
2026-05-02 11:02:13 -0300

    feat(lib): implement getPackageInfo for project metadata

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,4 +2,5 @@ export * from './platform/index.js';
 export * from './paths/index.js';
 export * from './arg-parser.js';
 export * from './logger/index.js';
+export * from './package-info/index.js';
 export * from './errors/index.js';
